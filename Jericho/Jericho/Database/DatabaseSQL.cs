d4277d08d52927dc82a11e0214327d9d617bc1d7
IronHead <rouninlopez@gmail.com>
2020-09-19 19:57:00 -0500

    created Jericho.mdf

---
+++ Jericho/Jericho/Database/DatabaseSQL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jericho.Database
+{
+    public class DatabaseSQL
+    {
+        #region Variables
+        #endregion
+        #region Parameters
+        #endregion
+        #region Constructors
+        #endregion
+        #region Functions
+        #endregion
+    }
+}
